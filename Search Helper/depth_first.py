459e101c0e7fface2064e2b6eb0ba6488c94c56c
Shemar Jones <jones.shemar.sj@gmail.com>
2021-06-20 12:11:46 -0500

    Completed Depth First Traversal
    It does what it needs to do according to Stones' lecture video. And it also prints the path and returns the helper 'diagram' thing that Stone used as an example.

---
+++ Search Helper/depth_first.py
@@ -0,0 +1,44 @@
+
+
+
+# Depth First Traversal
+def depth_first(start, graph, goal):
+    
+    # fringe is a stack
+    fringe = list(start)
+    stack = list(start)
+    path = list()
+
+    # Begin Loop
+    while len(stack) != 0:
+        next_node = stack.pop(0)
+        path.append(next_node)
+
+        # Check for goal in the Stack
+        if next_node == goal:
+            break
+
+        # Get the next lexicographic node children
+        node_children = sorted(graph[next_node])
+
+        fringe += ["|"] + node_children + stack
+        stack = node_children + stack
+
+    print(f"| The PATH: {path}")
+    return fringe
+
+#--------------------------------------------------------
+if __name__ == "__main__":
+    graph = {
+        "A": ["G", "D", "B"],
+        "B": ["F", "E"],
+        "C": ["D", "H"],
+        "D": ["G"],
+        "E": [],
+        "F": ["D", "C"],
+        "G": [],
+        "H": []
+    }
+
+    result = depth_first("A", graph, "G")
+    print(result)
