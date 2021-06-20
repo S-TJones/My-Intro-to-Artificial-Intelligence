ac52509e319c5e2ba1674bc3eab81dffb66d429e
Shemar Jones <jones.shemar.sj@gmail.com>
2021-06-20 16:21:54 -0500

    Updated Breadth First Traversal
    I wasn't satisfied with the previous function and how I went about doing it. Especially since I know that I didn't do it the right way by checking every node on each level. So that's what I did. I created a separate function to that though and I managed to make it output the correct path.

--- Search Helper/breadth_first.py
+++ Search Helper/breadth_first.py
@@ -3,6 +3,40 @@ from visual_helper import *
 
 
 # Expand shallowest unexpanded node
+def breadth_first_revamped(start, graph, goal):
+
+    # Should store each list of children per height/level of the tree
+    each_row = list()
+    all_paths = list(start)
+    new_paths = list()
+
+    current_neighbors = sorted(graph[start])
+    each_row.append(current_neighbors)
+
+    while len(each_row) != 0:
+        next_neighbors = set()
+        new_paths = list()
+
+        for path_node in all_paths:
+            next_node = path_node[-1]
+            current_neighbors = sorted(graph[next_node])
+
+            if goal in current_neighbors:
+                new_paths = path_node + goal
+                break
+
+            for node in current_neighbors:
+                new_path = path_node + node
+                new_paths.append(new_path)
+
+            next_neighbors.update(current_neighbors)
+
+        all_paths = new_paths
+        each_row = list(next_neighbors)
+
+    return get_path(list(new_paths))
+
+# Expand shallowest unexpanded node
 def breadth_first(start, graph, goal):
     
     # fringe is a FIFO Queue
@@ -47,5 +81,7 @@ if __name__ == "__main__":
         "H": []
     }
 
+    result = breadth_first_revamped("A", graph, "H")
+    print("| PATH: ", result)
     result = breadth_first("A", graph, "H")
     print("| HELPER: ", result)
