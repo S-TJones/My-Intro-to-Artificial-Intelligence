5c9a2708c75e3b46b6c0bcccbca8ef10b794b10c
Shemar Jones <jones.shemar.sj@gmail.com>
2021-06-23 18:52:16 -0500

    Add a separate graph for Test Graphs
    I basically put all graphs in one python file because the functions may re-use the same graphs. This makes it easier as well.

--- Search Helper/a_star.py
+++ Search Helper/a_star.py
@@ -2,10 +2,11 @@
 
 from visual_helper import *
 from uniform_cost import *
+from test_graphs import *
 
 
 # It's more of a Dijkstra to me
-def a_star(start, graph, node_price, goal):
+def a_star(start, graph, node_heuristics, goal):
 
     visited = set()
     fringe = list()
@@ -16,7 +17,6 @@ def a_star(start, graph, node_price, goal):
     total = 0
 
     next_node = (start, 0, 0, path)
-    print()
 
     while True:
         node = next_node[0]
@@ -37,7 +37,7 @@ def a_star(start, graph, node_price, goal):
         for ele in node_neighbors:
             key = ele[0]
             key_value = ele[1]
-            heuristic = node_price[key]
+            heuristic = node_heuristics[key]
             new_value = (key_value + weight) + heuristic
 
             new_node = (key, new_value, key_value+weight, new_path+key)
@@ -61,7 +61,6 @@ def a_star(start, graph, node_price, goal):
         key = next_node[0]
 
         path_expanded += key
-        print(f"| Smallest = ({key}, {next_node[2]})")
         
         # Pop the smallest, in this case remove it
         queue.remove(next_node)
@@ -76,60 +75,13 @@ def a_star(start, graph, node_price, goal):
 #--------------------------------------------------------
 if __name__ == "__main__":
 
-    graph = {
-        "A": [("Z", 75), ("T", 118), ("S", 140)],
-        "B": [("P", 101), ("F", 211), ("G", 90), ("U", 85)],
-        "C": [("D", 120), ("R", 146), ("P", 138)],
-        "D": [("M", 75), ("C", 120)],
-        "E": [("H", 86)],
-        "F": [("S", 99), ("B", 211)],
-        "G": [("B", 90)],
-        "H": [("U", 98), ("E", 86)],
-        "I": [("V", 92)],
-        "J": [], # Empty -
-        "K": [], # Empty -
-        "L": [("M", 70), ("T", 111)],
-        "M": [("D", 75), ("L", 70)],
-        "N": [("I", 87)],
-        "O": [("Z", 71), ("S", 151)],
-        "P": [("R", 97), ("C", 138), ("B", 101)],
-        "Q": [], # Empty -
-        "R": [("S", 80), ("C", 148), ("P", 97)], # C-146?
-        "S": [("O", 151), ("A", 140), ("R", 80), ("F", 99)],
-        "T": [("A", 118), ("L", 111)],
-        "U": [("B", 85), ("H", 98), ("V", 142)],
-        "V": [("I", 92), ("U", 142)],
-        "W": [], # Empty -
-        "X": [], # Empty -
-        "Y": [], # Empty -
-        "Z": [("O", 71), ("A", 75)]
-    }
+    result = a_star("A", tutorial3_graph, tutorial3_heuristics, "B")
 
-    node_price = {
-        "A": 366,
-        "B": 0,
-        "C": 160,
-        "D": 242,
-        "E": 161,
-        "F": 176,
-        "G": 77,
-        "H": 151,
-        "I": 226,
-        "L": 244,
-        "M": 241,
-        "N": 234,
-        "O": 380,
-        "P": 100,
-        "R": 193,
-        "S": 253,
-        "T": 329,
-        "U": 80,
-        "V": 199,
-        "Z": 374
-    }
+    # Assignment Tests
+    # result = a_star("A", graphT1, heurist_graphT1, "G") #Expects ['A', 'B', 'F', 'D', 'G']
+    # result = a_star("A", graphT2, heurist_graphT2, "B") #Expects ['A', 'S', 'R', 'P', 'B']
+    # result = a_star("T", graphT2, heurist_graphT2, "B") #Expects ['T', 'A', 'S', 'R', 'P', 'B']
 
-    
-    result = a_star("A", graph, node_price, "B")
     print()
     print(f"| The FRINGE: ")
     for ele in result:
